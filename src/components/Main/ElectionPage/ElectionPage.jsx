eebbad21376f917f89085120ba0eae12e8d967c7
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-07-06 12:57:09 +0200

    v. 1.2.3 - toISOstring + maxDate

--- src/components/Main/ElectionPage/ElectionPage.jsx
+++ src/components/Main/ElectionPage/ElectionPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './ElectionPage.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -7,14 +7,14 @@ import moment from 'moment'
 
 
 const ElectionPage = () => {
-  const [endDate, setEndDate] = useState(moment().subtract(1, 'months').toDate());
-  const [startDate, setStartDate] = useState(moment().toDate());
+  const [endDate, setEndDate] = useState(moment().toDate());
+  const [startDate, setStartDate] = useState(moment().subtract(1, 'months').toDate());
   const [results, setResults] = useState(null);
-  const fethArticles = () => {
-    fetch(`http://localhost:4000/election?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`)
+  const fethArticles = useCallback(() => {
+    fetch(`http://localhost:4000/election?to=${startDate.toISOString()}&from=${endDate.toISOString()}`)
       .then((response) => response.json())
       .then((res) => setResults(res));
-  }
+  }, [startDate, endDate]);
 
 useEffect (() => { fethArticles(); }, []);
 useEffect (() => { fethArticles(); }, [startDate, endDate]);
@@ -25,11 +25,21 @@ return (
   <div className="ElectionPage">
     <div>
       <label>Data początkowa: </label>
-      <DatePicker selected={startDate} onChange={setStartDate} dateFormat="dd-MM-yyyy"/>
+      <DatePicker
+        selected={startDate}
+        onChange={setStartDate}
+        dateFormat="dd-MM-yyyy"
+        maxDate={ new Date() }
+/>
     </div>
     <div>
       <label>Data końcowa: </label>
-      <DatePicker selected={endDate} onChange={setEndDate} dateFormat="dd-MM-yyyy"/>
+      <DatePicker
+        selected={endDate}
+        onChange={setEndDate}
+        dateFormat="dd-MM-yyyy"
+        maxDate={ new Date() }
+        />
     </div>
     {results ? (<NewsList articles={results.articles} />) : null }
   </div>
