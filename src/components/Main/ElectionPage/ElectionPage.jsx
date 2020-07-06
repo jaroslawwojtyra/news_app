b5f6d90a7f9b4996e18d3498c67af95c3bbd2fae
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-07-06 11:48:01 +0200

    v.1.2.0 - add moment to data picker

--- src/components/Main/ElectionPage/ElectionPage.jsx
+++ src/components/Main/ElectionPage/ElectionPage.jsx
@@ -3,11 +3,12 @@ import './ElectionPage.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NewsList from '../HomePage/NewsList/NewsList'
+import moment from 'moment'
 
 
 const ElectionPage = () => {
-  const [endDate, setEndDate] = useState(new Date());
-  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(moment().subtract(1, 'months').toDate());
+  const [startDate, setStartDate] = useState(moment().toDate());
   const [results, setResults] = useState(null);
 
 useEffect (() => {
