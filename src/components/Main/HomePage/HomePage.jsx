61b8cef96f3f1aa95c6bbabe24f6164e7d10dfd3
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-08-21 10:35:41 +0200

    v. 1.6.3 - paggination todos

--- src/components/Main/HomePage/HomePage.jsx
+++ src/components/Main/HomePage/HomePage.jsx
@@ -23,22 +23,27 @@ componentDidMount() {
 
 componentDidUpdate(prevProps, prevState, snapshot) {
   if
-  (prevState.category !== this.state.category ||
-    prevState.lang !== this.state.lang) {
+  (prevState.category !== this.state.category || prevState.lang !== this.state.lang) {
     this.getArticles();
   }
   if (prevState.lang !== this.context) this.setState({ lang: this.context });
+//TODO: get articles jak strona się różni od poprzedniej
 }
 
 getArticles(){
   const { category } = this.state;
   const query = category ? `&category=${category}` : '';
+  //TODO dodanie do wuery aktualnej strony zgodnie z dokumentacją newsapi
   fetch(`http://localhost:4000/articles?country=${this.context}${query}`)
     .then((response) => response.json())
     .then((results) => this.setState({ results }));
 }
 
 setCategory = (category) => this.setState({ category });
+onPageChange = (e, {activePage}) =>(
+  console.log("test")
+  //TODO: ustawienie w stanie aktywnej strony
+);
 
 render() {
   const { results } = this.state;
@@ -50,6 +55,7 @@ render() {
         <Pagination
           defaultActivePage={1}
           totalPages={ Math.ceil(results.totalResults / 20) }
+          onPageChange={this.onPageChange}
           />) : null }
       <NewsList articles={results.articles} />
     </div>
